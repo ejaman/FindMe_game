d921bad565369dd64126ed7659002a92df067848
ejaman <leegm042@gmail.com>
2021-09-08 14:59:06 +0900

    change css

--- find_d_color/main.js
+++ find_d_color/main.js
@@ -1,18 +1,4 @@
 const special = document.querySelector('.special');
-special.addEventListener('click', (event) => {
-  console.log(`rect: ${special.getBoundingClientRect()}`);
-  console.log(`page x,y: ${event.pageX}, ${event.pageY}`);
-  console.log(`client x,y: ${event.clientX}, ${event.clientY}`);
+special.addEventListener('click', (event) =>{
+  console.log('click');
 });
-
-function by100() {
-  scrollBy({top:100, left:0, behavior: "smooth"});
-}
-function to100() {
-  scrollTo({top:100, behavior:"smooth"});
-}
-function intoSpecial() {
-  // const loca = special.getBoundingClientRect().top;
-  // scrollTo(0,loca);
-  special.scrollIntoView({behavior:"smooth"});
-}
